ba0fdddc7bb912c88f7fee21e363bc7cfdc8a0e3
trobert2000 <contractor@imbium.de>
2019-12-27 19:44:38 +0100

    erster

---
+++ src/de/imbium/telitom/TeliTomCl.java
@@ -0,0 +1,17 @@
+package de.imbium.telitom;
+
+public class TeliTomCl {
+	
+	public TeliTomCl() {
+		System.out.println("Hello");
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TeliTomCl tx = new TeliTomCl();
+		
+	}
+
+}
